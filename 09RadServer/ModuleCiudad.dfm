7aa9f5f7f39cffe1f1e44713f7a8c4c712c94693
Jorge Arbelaez <jearbelaezr@audisoft.com>
2019-12-27 16:36:16 -0500

    Retorno de tabla en formato JSON

--- 09RadServer/ModuleCiudad.dfm
+++ 09RadServer/ModuleCiudad.dfm
@@ -2,4 +2,71 @@ object CiudadResource: TCiudadResource
   OldCreateOrder = False
   Height = 300
   Width = 600
+  object FDConnection: TFDConnection
+    Params.Strings = (
+      'Database=CursoBdDelphi'
+      'User_Name=sa'
+      'Password=AudiSoft123'
+      'Server=localhost'
+      'DriverID=MSSQL')
+    Connected = True
+    LoginPrompt = False
+    Left = 48
+    Top = 24
+  end
+  object FDQueryCiudades: TFDQuery
+    Connection = FDConnection
+    SQL.Strings = (
+      'select * from ciudad')
+    Left = 48
+    Top = 104
+    object FDQueryCiudadesID_CIUDAD: TIntegerField
+      FieldName = 'ID_CIUDAD'
+      Required = True
+    end
+    object FDQueryCiudadesNOMBRE_CIUDAD: TStringField
+      FieldName = 'NOMBRE_CIUDAD'
+      Required = True
+      Size = 100
+    end
+  end
+  object FDGUIxWaitCursor: TFDGUIxWaitCursor
+    Provider = 'Console'
+    Left = 160
+    Top = 24
+  end
+  object FDPhysMSSQLDriverLink: TFDPhysMSSQLDriverLink
+    Left = 280
+    Top = 24
+  end
+  object FDStanStorageJSONLink: TFDStanStorageJSONLink
+    Left = 424
+    Top = 24
+  end
+  object FDQueryCiudad: TFDQuery
+    Connection = FDConnection
+    SQL.Strings = (
+      'select * from ciudad where id_ciudad = :id_ciudad')
+    Left = 192
+    Top = 104
+    ParamData = <
+      item
+        Name = 'ID_CIUDAD'
+        DataType = ftInteger
+        ParamType = ptInput
+        Value = 1
+      end>
+    object FDQueryCiudadID_CIUDAD: TIntegerField
+      FieldName = 'ID_CIUDAD'
+      Origin = 'ID_CIUDAD'
+      ProviderFlags = [pfInUpdate, pfInWhere, pfInKey]
+      Required = True
+    end
+    object FDQueryCiudadNOMBRE_CIUDAD: TStringField
+      FieldName = 'NOMBRE_CIUDAD'
+      Origin = 'NOMBRE_CIUDAD'
+      Required = True
+      Size = 100
+    end
+  end
 end
--- 09RadServer/ModuleCiudad.pas
+++ 09RadServer/ModuleCiudad.pas
@@ -6,97 +6,174 @@ interface
 
 uses
   System.SysUtils, System.Classes, System.JSON,
-  EMS.Services, EMS.ResourceAPI, EMS.ResourceTypes;
+  EMS.Services, EMS.ResourceAPI, EMS.ResourceTypes, FireDAC.Stan.Intf,
+  FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf,
+  FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys,
+  FireDAC.Phys.MSSQL, FireDAC.Phys.MSSQLDef, FireDAC.Stan.Param, FireDAC.DatS,
+  FireDAC.DApt.Intf, FireDAC.DApt, FireDAC.ConsoleUI.Wait,
+  FireDAC.Stan.StorageJSON, FireDAC.Phys.ODBCBase, FireDAC.Comp.UI, Data.DB,
+  FireDAC.Comp.DataSet, FireDAC.Comp.Client;
 
 type
+
   [ResourceName('Ciudad')]
   TCiudadResource = class(TDataModule)
+    FDConnection: TFDConnection;
+    FDQueryCiudades: TFDQuery;
+    FDGUIxWaitCursor: TFDGUIxWaitCursor;
+    FDPhysMSSQLDriverLink: TFDPhysMSSQLDriverLink;
+    FDStanStorageJSONLink: TFDStanStorageJSONLink;
+    FDQueryCiudad: TFDQuery;
+    FDQueryCiudadID_CIUDAD: TIntegerField;
+    FDQueryCiudadNOMBRE_CIUDAD: TStringField;
+    FDQueryCiudadesID_CIUDAD: TIntegerField;
+    FDQueryCiudadesNOMBRE_CIUDAD: TStringField;
   published
 
-    [EndPointRequestSummary('Ejemplo', 'Listar', 'Recupera la lista de ciudades', 'application/json', '')]
-    [EndPointResponseDetails(200, 'Ok', TAPIDoc.TPrimitiveType.spObject, TAPIDoc.TPrimitiveFormat.None, '', '')]
-    procedure Get(const AContext: TEndpointContext; const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
+    [EndPointRequestSummary('Ejemplo', 'Listar',
+      'Recupera la lista de ciudades', 'application/json', '')]
+    [EndPointResponseDetails(200, 'Ok', TAPIDoc.TPrimitiveType.spObject,
+      TAPIDoc.TPrimitiveFormat.None, '', '')]
+    procedure Get(const AContext: TEndpointContext;
+      const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
 
-    [EndPointRequestSummary('Ejemplo', 'Consultar', 'Recupera una ciudad a partir del código', 'application/json', '')]
-    [EndPointRequestParameter(TAPIDocParameter.TParameterIn.Path, 'item', 'Código de la ciudad', true, TAPIDoc.TPrimitiveType.spString,
+    [EndPointRequestSummary('Ejemplo', 'Consultar',
+      'Recupera una ciudad a partir del código', 'application/json', '')]
+    [EndPointRequestParameter(TAPIDocParameter.TParameterIn.Path, 'item',
+      'Código de la ciudad', true, TAPIDoc.TPrimitiveType.spString,
       TAPIDoc.TPrimitiveFormat.None, TAPIDoc.TPrimitiveType.spString, '', '')]
-    [EndPointResponseDetails(200, 'Ok', TAPIDoc.TPrimitiveType.spObject, TAPIDoc.TPrimitiveFormat.None, '', '')]
-    [EndPointResponseDetails(404, 'Not Found', TAPIDoc.TPrimitiveType.spNull, TAPIDoc.TPrimitiveFormat.None, '', '')]
+    [EndPointResponseDetails(200, 'Ok', TAPIDoc.TPrimitiveType.spObject,
+      TAPIDoc.TPrimitiveFormat.None, '', '')]
+    [EndPointResponseDetails(404, 'Not Found', TAPIDoc.TPrimitiveType.spNull,
+      TAPIDoc.TPrimitiveFormat.None, '', '')]
     [ResourceSuffix('{item}')]
-    procedure GetItem(const AContext: TEndpointContext; const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
+    procedure GetItem(const AContext: TEndpointContext;
+      const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
 
-    [EndPointRequestSummary('Tests', 'PostItem', 'Creates new item', '', 'application/json')]
-    [EndPointRequestParameter(TAPIDocParameter.TParameterIn.Body, 'body', 'A new item content', true, TAPIDoc.TPrimitiveType.spObject,
+    [EndPointRequestSummary('Tests', 'PostItem', 'Creates new item', '',
+      'application/json')]
+    [EndPointRequestParameter(TAPIDocParameter.TParameterIn.Body, 'body',
+      'A new item content', true, TAPIDoc.TPrimitiveType.spObject,
       TAPIDoc.TPrimitiveFormat.None, TAPIDoc.TPrimitiveType.spObject, '', '')]
-    [EndPointResponseDetails(200, 'Ok', TAPIDoc.TPrimitiveType.spNull, TAPIDoc.TPrimitiveFormat.None, '', '')]
-    [EndPointResponseDetails(409, 'Item Exist', TAPIDoc.TPrimitiveType.spNull, TAPIDoc.TPrimitiveFormat.None, '', '')]
-    procedure Post(const AContext: TEndpointContext; const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
+    [EndPointResponseDetails(200, 'Ok', TAPIDoc.TPrimitiveType.spNull,
+      TAPIDoc.TPrimitiveFormat.None, '', '')]
+    [EndPointResponseDetails(409, 'Item Exist', TAPIDoc.TPrimitiveType.spNull,
+      TAPIDoc.TPrimitiveFormat.None, '', '')]
+    procedure Post(const AContext: TEndpointContext;
+      const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
 
-    [EndPointRequestSummary('Tests', 'PutItem', 'Updates item with specified ID', '', 'application/json')]
-    [EndPointRequestParameter(TAPIDocParameter.TParameterIn.Path, 'item', 'A item ID', true, TAPIDoc.TPrimitiveType.spString,
+    [EndPointRequestSummary('Tests', 'PutItem',
+      'Updates item with specified ID', '', 'application/json')]
+    [EndPointRequestParameter(TAPIDocParameter.TParameterIn.Path, 'item',
+      'A item ID', true, TAPIDoc.TPrimitiveType.spString,
       TAPIDoc.TPrimitiveFormat.None, TAPIDoc.TPrimitiveType.spString, '', '')]
-    [EndPointRequestParameter(TAPIDocParameter.TParameterIn.Body, 'body', 'A item changes', true, TAPIDoc.TPrimitiveType.spObject,
+    [EndPointRequestParameter(TAPIDocParameter.TParameterIn.Body, 'body',
+      'A item changes', true, TAPIDoc.TPrimitiveType.spObject,
       TAPIDoc.TPrimitiveFormat.None, TAPIDoc.TPrimitiveType.spObject, '', '')]
-    [EndPointResponseDetails(200, 'Ok', TAPIDoc.TPrimitiveType.spNull, TAPIDoc.TPrimitiveFormat.None, '', '')]
-    [EndPointResponseDetails(404, 'Not Found', TAPIDoc.TPrimitiveType.spNull, TAPIDoc.TPrimitiveFormat.None, '', '')]
+    [EndPointResponseDetails(200, 'Ok', TAPIDoc.TPrimitiveType.spNull,
+      TAPIDoc.TPrimitiveFormat.None, '', '')]
+    [EndPointResponseDetails(404, 'Not Found', TAPIDoc.TPrimitiveType.spNull,
+      TAPIDoc.TPrimitiveFormat.None, '', '')]
     [ResourceSuffix('{item}')]
-    procedure PutItem(const AContext: TEndpointContext; const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
+    procedure PutItem(const AContext: TEndpointContext;
+      const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
 
-    [EndPointRequestSummary('Tests', 'DeleteItem', 'Deletes item with specified ID', '', '')]
-    [EndPointRequestParameter(TAPIDocParameter.TParameterIn.Path, 'item', 'A item ID', true, TAPIDoc.TPrimitiveType.spString,
+    [EndPointRequestSummary('Tests', 'DeleteItem',
+      'Deletes item with specified ID', '', '')]
+    [EndPointRequestParameter(TAPIDocParameter.TParameterIn.Path, 'item',
+      'A item ID', true, TAPIDoc.TPrimitiveType.spString,
       TAPIDoc.TPrimitiveFormat.None, TAPIDoc.TPrimitiveType.spString, '', '')]
-    [EndPointResponseDetails(200, 'Ok', TAPIDoc.TPrimitiveType.spNull, TAPIDoc.TPrimitiveFormat.None, '', '')]
-    [EndPointResponseDetails(404, 'Not Found', TAPIDoc.TPrimitiveType.spNull, TAPIDoc.TPrimitiveFormat.None, '', '')]
+    [EndPointResponseDetails(200, 'Ok', TAPIDoc.TPrimitiveType.spNull,
+      TAPIDoc.TPrimitiveFormat.None, '', '')]
+    [EndPointResponseDetails(404, 'Not Found', TAPIDoc.TPrimitiveType.spNull,
+      TAPIDoc.TPrimitiveFormat.None, '', '')]
     [ResourceSuffix('{item}')]
-    procedure DeleteItem(const AContext: TEndpointContext; const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
+    procedure DeleteItem(const AContext: TEndpointContext;
+      const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
+  private
+    function JsonCiudad(FDQuery: TFDQuery): TJSONObject; overload;
+    function JsonCiudad(IdCiudad: String): TJSONObject; overload;
   end;
 
 implementation
 
 {%CLASSGROUP 'System.Classes.TPersistent'}
-
 {$R *.dfm}
 
-procedure TCiudadResource.Get(const AContext: TEndpointContext; const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
-begin
-  // Sample code
-  AResponse.Body.SetValue(TJSONString.Create('Ciudad'), True)
-end;
-
-procedure TCiudadResource.GetItem(const AContext: TEndpointContext; const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
+procedure TCiudadResource.Get(const AContext: TEndpointContext;
+  const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
 var
-  LItem: string;
+  JsonArray: TJSONArray;
 begin
-  LItem := ARequest.Params.Values['item'];
-  // Sample code
-  AResponse.Body.SetValue(TJSONString.Create('Ciudad ' + LItem), True)
+  try
+    GetItem(AContext, ARequest, AResponse);
+  except
+    JsonArray:= TJSONArray.Create;
+    FDQueryCiudades.Close;
+    FDQueryCiudades.Open;
+    while not FDQueryCiudades.Eof do
+    begin
+      JsonArray.Add(JsonCiudad(FDQueryCiudades));
+      FDQueryCiudades.Next;
+    end;
+    AResponse.Body.SetValue(JsonArray, True);
+  end;
 end;
 
-procedure TCiudadResource.Post(const AContext: TEndpointContext; const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
-begin
-end;
-
-procedure TCiudadResource.PutItem(const AContext: TEndpointContext; const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
+procedure TCiudadResource.GetItem(const AContext: TEndpointContext;
+  const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
 var
-  LItem: string;
+  LCodigo: string;
+  JSonObject: TJSONObject;
 begin
-  LItem := ARequest.Params.Values['item'];
+  LCodigo := ARequest.Params.Values['item'];
+  JSonObject:= JsonCiudad(LCodigo);
+  AResponse.Body.SetValue(JSonObject, true)
 end;
 
-procedure TCiudadResource.DeleteItem(const AContext: TEndpointContext; const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
-var
-  LItem: string;
+function TCiudadResource.JsonCiudad(IdCiudad: String): TJSONObject;
 begin
-  LItem := ARequest.Params.Values['item'];
+  FDQueryCiudad.Close;
+  FDQueryCiudad.ParamByName('id_ciudad').AsString := IdCiudad;
+  FDQueryCiudad.Open;
+  Result:= JsonCiudad(FDQueryCiudad);
+end;
+
+function TCiudadResource.JsonCiudad(FDQuery: TFDQuery): TJSONObject;
+begin
+  Result := TJSONObject.Create;
+  Result.AddPair('Codigo', FDQuery.FieldByName('id_ciudad').AsString);
+  Result.AddPair('Nombre', FDQuery.FieldByName('nombre_ciudad').AsString);
+end;
+
+procedure TCiudadResource.Post(const AContext: TEndpointContext;
+  const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
+begin
+end;
+
+procedure TCiudadResource.PutItem(const AContext: TEndpointContext;
+  const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
+var
+  LCodigo: string;
+begin
+  LCodigo := ARequest.Params.Values['item'];
+end;
+
+procedure TCiudadResource.DeleteItem(const AContext: TEndpointContext;
+  const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
+var
+  LCodigo: string;
+begin
+  LCodigo := ARequest.Params.Values['item'];
 end;
 
 procedure Register;
 begin
- RegisterResource(TypeInfo(TCiudadResource));
+  RegisterResource(TypeInfo(TCiudadResource));
 end;
 
 initialization
-  Register;
+
+Register;
+
 end.
-
-
